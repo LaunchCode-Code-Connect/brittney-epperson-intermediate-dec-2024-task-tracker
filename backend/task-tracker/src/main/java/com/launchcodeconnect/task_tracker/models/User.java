07996d8c022872b37264b5604c4089c2b69c114f
Brittney Epperson <beppers87@hotmail.com>
2024-12-05 14:52:16 -0600

    Updated User model to implement UserDetails interface

--- backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/models/User.java
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/models/User.java
@@ -6,11 +6,16 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class User extends AbstractEntity {
+public class User extends AbstractEntity implements UserDetails {
 
     @NotNull
     @Column(name = "username")
@@ -40,5 +45,31 @@ public class User extends AbstractEntity {
 
     public boolean isPasswordEmpty() {
         return getPassword() == null || getPassword().isEmpty();
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
