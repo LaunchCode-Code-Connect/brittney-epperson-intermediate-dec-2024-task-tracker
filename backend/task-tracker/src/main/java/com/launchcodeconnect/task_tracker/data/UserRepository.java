de044e0a00cf83feeaf86fa2ad5482d86fdde696
Brittney Epperson <beppers87@hotmail.com>
2024-12-05 13:55:32 -0600

    Added User repo

--- backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/data/UserRepository.java
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/data/UserRepository.java
@@ -1,4 +1,12 @@
 package com.launchcodeconnect.task_tracker.data;
 
-public interface UserRepository {
+import com.launchcodeconnect.task_tracker.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername (String username);
+
+    User findByEmail (String email);
+
 }
