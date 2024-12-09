4b7a6adeb402d1b296ee1d318adcd5d92ac3c08c
Brittney Epperson <beppers87@hotmail.com>
2024-12-09 11:31:16 -0600

    Added missing annotation

--- backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/data/UserRepository.java
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/data/UserRepository.java
@@ -2,7 +2,9 @@ package com.launchcodeconnect.task_tracker.data;
 
 import com.launchcodeconnect.task_tracker.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername (String username);
