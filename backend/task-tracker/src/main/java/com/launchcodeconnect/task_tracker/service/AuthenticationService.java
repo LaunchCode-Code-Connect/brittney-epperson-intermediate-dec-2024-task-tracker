7cb127f308210c14af496bd3fea190537cf4ca30
Brittney Epperson <beppers87@hotmail.com>
2024-12-10 13:29:32 -0600

    Added Authentication Service

--- backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/service/AuthenticationService.java
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/service/AuthenticationService.java
@@ -1,4 +1,53 @@
 package com.launchcodeconnect.task_tracker.service;
 
+import com.launchcodeconnect.task_tracker.data.UserRepository;
+import com.launchcodeconnect.task_tracker.models.User;
+import com.launchcodeconnect.task_tracker.models.dto.LoginFormDTO;
+import com.launchcodeconnect.task_tracker.models.dto.RegisterFormDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterFormDTO input) {
+        User user = new User(
+                input.getUsername(),
+                input.getEmail(),
+                passwordEncoder.encode(input.getPassword())
+        );
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginFormDTO input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+
 }
