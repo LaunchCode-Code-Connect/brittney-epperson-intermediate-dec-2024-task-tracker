f60e15ae7dbf0a7cfed0bf466826947466a13e9f
Brittney Epperson <beppers87@hotmail.com>
2024-12-12 14:02:47 -0600

    Added AuthContext for authentication, login, and logout

---
+++ frontend/task-tracker/app/context/AuthContext.js
@@ -0,0 +1,78 @@
+'use client';
+
+import { createContext, useContext, useState, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import { fetchClient } from '../utils/fetchClient';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+		checkAuthentication();
+	}, []);
+
+    const checkAuthentication = async () => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            try {
+                const decodedToken = jwtDecode(token);
+                const username = decodedToken.sub;
+                if (username) {
+                    setUser({ username });
+                } else { 
+                    setUser(null);
+                }
+            } catch (error) {
+                console.error('Error verifying token', error);
+                setUser(null);
+            }
+        } else {
+            setUser(null);
+        }
+    };
+
+    const login = async (email, password) => {
+		setError(null);
+
+		try {
+			const data = await fetchClient('/login', {
+				method: 'POST',
+				body: JSON.stringify({ email, password }),
+			});
+
+			const token = data.token;
+			localStorage.setItem('token', token);
+
+			const decodedToken = jwtDecode(token);
+			const username = decodedToken.sub;
+
+			setUser({ username });
+			return { success: true, username };
+		} catch (error) {
+			setError(error.message);
+			return { success: false, error: error.message };
+		}
+	};
+
+	const logout = () => {
+		localStorage.removeItem('token');
+		setUser(null);
+	};
+
+    return (
+		<AuthContext.Provider
+			value={{ user, login, logout, checkAuthentication, error }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export const useAuth = () => {
+	return useContext(AuthContext);
+};
+
+export { AuthContext };
