ec1babd492ce5ae3f1bf8955defb34916d16e698
Brittney Epperson <beppers87@hotmail.com>
2024-12-18 17:08:57 -0600

    Added UserService

---
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/service/UserService.java
@@ -0,0 +1,19 @@
+package com.launchcodeconnect.task_tracker.service;
+
+import com.launchcodeconnect.task_tracker.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+
+}
