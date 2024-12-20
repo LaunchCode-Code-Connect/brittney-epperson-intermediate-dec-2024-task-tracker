00f56840b07eb004dfeebd63e33d19cd982d16b3
Brittney Epperson <beppers87@hotmail.com>
2024-12-20 13:09:50 -0600

    Added PUT mapping to edit users

--- backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/controllers/UserController.java
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/controllers/UserController.java
@@ -3,12 +3,17 @@ package com.launchcodeconnect.task_tracker.controllers;
 
 import com.launchcodeconnect.task_tracker.data.UserRepository;
 import com.launchcodeconnect.task_tracker.models.User;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-        import java.util.List;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -30,6 +35,34 @@ public class UserController {
         return userRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable("id") int id, @RequestBody @Valid User updatedUser, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.unprocessableEntity().body(errors);
+        }
+
+        Optional<User> existingUser = userRepository.findById(id);
+        if (existingUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User userToUpdate = existingUser.get();
+        userToUpdate.setUsername(updatedUser.getUsername());
+
+        // Check if the password is not empty in the updatedUser
+        if (!updatedUser.isPasswordEmpty()) {
+            // Update the password only if it's not empty
+            userToUpdate.setPassword(updatedUser.getPassword());
+        }
+
+        userRepository.save(userToUpdate);
+        return ResponseEntity.ok(userToUpdate);
     }
 
     @DeleteMapping("/{id}")
