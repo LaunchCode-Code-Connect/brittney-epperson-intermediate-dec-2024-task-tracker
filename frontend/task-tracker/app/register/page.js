d895ebffaf958c80627adf1f7cfb7814b5abc84d
Brittney Epperson <beppers87@hotmail.com>
2024-12-20 11:04:35 -0600

    Removed unused checkbox for Terms & Conditions

--- frontend/task-tracker/app/register/page.js
+++ frontend/task-tracker/app/register/page.js
@@ -214,31 +214,6 @@ export default function Page() {
 									</p>
 								)}
 							</div>
-							{/* <div className='flex items-start'>
-								<div className='flex items-center h-5'>
-									<input
-										id='terms'
-										aria-describedby='terms'
-										type='checkbox'
-										className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800'
-										required=''
-									/>
-								</div>
-								<div className='ml-3 text-sm'>
-									<label
-										htmlFor='terms'
-										className='font-light text-gray-500 dark:text-gray-300'
-									>
-										I accept the{' '}
-										<a
-											className='font-medium text-primary-600 hover:underline dark:text-primary-500'
-											href='#'
-										>
-											Terms and Conditions
-										</a>
-									</label>
-								</div>
-							</div> */}
 							{errors.form && (
 								<div
 									className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'
