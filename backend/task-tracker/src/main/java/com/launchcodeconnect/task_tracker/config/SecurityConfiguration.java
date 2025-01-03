5654babb997842420588cfe83d162918f6ce5353
Brittney Epperson <beppers87@hotmail.com>
2025-01-03 13:03:40 -0600

    Updated CORS to allow PATCH

--- backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/config/SecurityConfiguration.java
+++ backend/task-tracker/src/main/java/com/launchcodeconnect/task_tracker/config/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
                 "Authorization",
                 "Content-Type",
